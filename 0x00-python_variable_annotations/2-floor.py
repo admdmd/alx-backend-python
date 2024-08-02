a5475503186bd41b116668fbafbd57933e69ee94
admdmd <ademideadeboye@gmail.com>
2024-08-02 23:27:31 +0000

    comm

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+""" 2. Basic annotations - floor operation
+"""
 
-import math
 
-
-def floor(a: float) -> int:
-    '''Computes the floor of a floating-point number.
-    '''
-    return math.floor(a)
+def floor(n: float) -> int:
+    """ Returns floor of number
+    """
+    return int(n) if n >= 0 else int(n) - 1
